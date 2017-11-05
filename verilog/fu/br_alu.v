43a7bcf8d9fbdece51d83770b68de9f4ee1c3ddf
Jun Luo <juluo@umich.edu>
2017-11-05 15:15:38 -0500

    update tested submodule for FU

--- verilog/fu/br_alu.v
+++ verilog/fu/br_alu.v
@@ -29,16 +29,17 @@ endmodule // brcond
 
 module br_alu (
 
-		input				clk,rst,
-		input				start_i,
-		input		[63:0]  npc_i,
-		input		[63:0]  opa_i,//reg A value
-		input		[31:0]  inst_i,
-		input		[`ROB_IDX_W-1:0]	rob_idx_i,
+		input						clk,
+		input						rst,
+		input						start_i,
+		input		[63:0]  		npc_i,
+		input		[63:0]  		opa_i,//reg A value
+		input		[31:0]  		inst_i,
+		input		[`ROB_IDX_W-1:0]rob_idx_i,
 
-		output	logic			done_o,
-		output	logic	[63:0]	br_target_o,
-		output	logic			br_result_o,
+		output	logic				done_o,
+		output	logic	[63:0]		br_target_o,
+		output	logic				br_result_o,
 		output	logic	[`ROB_IDX_W-1:0]	rob_idx_o
 	
 		);
@@ -53,7 +54,8 @@ module br_alu (
 
 		assign br_disp = { {41{inst_i[20]}}, inst_i[20:0], 2'b00 };
 		assign br_result_nxt = (~cond_br) ? 1 : brcond_result;
-		assign rob_idx_nxt = rob_idx_i;	
+		assign rob_idx_nxt = rob_idx_i;
+		wire    [4:0] rb_idx = inst_i[20:16];	
 
 	always_comb begin
 		case({inst_i[31:29], 3'b0})
@@ -89,14 +91,13 @@ module br_alu (
 		.cond(brcond_result)
 		);
 		
-	always_ff (@posedge clk) begin
+	always_ff @(posedge clk) begin
 		if(rst) begin
 			done_o 		<= `SD 0;
 			br_result_o <= `SD 0;
 			br_target_o <= `SD 0;
 			rob_idx_o	<= `SD 0;
-		end
-		else
+		end else begin
 			done_o 		<= `SD done_nxt;
 			br_result_o <= `SD br_result_nxt;
 			br_target_o <= `SD br_target_nxt;
