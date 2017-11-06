c3f93de3cd9538432f69d3187b6366b2fbce64b2
Jun Luo <juluo@umich.edu>
2017-11-05 19:44:02 -0500

    modified rob part

--- verilog/core/core.v
+++ verilog/core/core.v
@@ -55,6 +55,34 @@ module core (
 	//---------------------------------------------------------------
 	// signals for rob
 	//---------------------------------------------------------------
+	logic	[`PRF_IDX_W-1:0]		fl2rob_tag_i;//tag sent from freelist
+	logic	[`PRF_IDX_W-2:0]		fl2rob_cur_head_i;//freelist head
+	logic	[`PRF_IDX_W-1:0]		map2rob_tag_i;//tag sent from maptable
+	logic	[`PRF_IDX_W-2:0]		decode2rob_logic_dest_i;//logic dest sent from decode
+	logic	[63:0]					decode2rob_PC_i;//instruction's PC sent from decode
+	logic							decode2rob_br_flag_i;//flag show whether the instruction is a branch
+	logic							decode2rob_br_pretaken_i;//branch predictor result sent from decode
+	logic	[63:0]					decode2rob_br_target_i;//branch target sent from decode 
+	logic							decode2rob_rd_mem_i;//flag shows whether this instruction read memory
+	logic							decode2rob_wr_mem_i;//flag shows whether this instruction write memory
+	logic							rob_dispatch_en_i;//signal from dispatch to allocate entry in rob
+	logic	[`BR_MASK_W-1:0]		decode2rob_br_mask_i;
+
+	logic	[`ROB_IDX_W:0]			fu2rob_idx_i;//tag sent from functional unit to know which entry's done register needed to be set 
+	logic							fu2rob_done_signal_i;//done signal from functional unit 
+	logic							fu2rob_br_taken_i;//branck taken result sent from functional unit
+
+	logic	[`HT_W-1:0]				rob2rs_tail_idx_o;//tail # sent to rs to record which entry the instruction is 
+	logic	[`PRF_IDX_W-1:0]		rob2fl_tag_o;//tag from ROB to freelist for returning the old tag to freelist 
+	logic	[`PRF_IDX_W-1:0]		rob2arch_map_tag_o;//tag from ROB to Arch map
+	logic	[`PRF_IDX_W-2:0]		rob2arch_map_logic_dest_o;//logic dest from ROB to Arch map
+	logic							rob_stall_dp_o;//signal show if the ROB is full
+	logic							rob_head_retire_rdy_o;//the head of ROb is ready to retire
+
+	logic							br_recovery_rdy_o;//ready to start early branch recovery
+	logic	[`PRF_IDX_W-2:0]		rob2fl_recover_head_o;
+	logic	[`BR_MASK_W-1:0]		br_recovery_mask_o;
+	logic	[`BR_STATE_W-1:0]		br_state_o;//branch state signal to branch mask
 
 
 	//---------------------------------------------------------------
@@ -128,6 +156,47 @@ module core (
 	// rs instantiation
 	//===============================================================
 	
+
+	//===============================================================
+	//rob instantiation
+	//===============================================================
+	rob rob1 (
+		.fl2rob_tag_i,//tag sent from freelist
+		.fl2rob_cur_head_i,//freelist head
+		.map2rob_tag_i,//tag sent from maptable
+		.decode2rob_logic_dest_i,//logic dest sent from decode
+		.decode2rob_PC_i,//instruction's PC sent from decode
+		.decode2rob_br_flag_i,//flag show whether the instruction is a branch
+		.decode2rob_br_pretaken_i,//branch predictor result sent from decode
+		.decode2rob_br_target_i,//branch target sent from decode 
+		.decode2rob_rd_mem_i,//flag shows whether this instruction read memory
+		.decode2rob_wr_mem_i,//flag shows whether this instruction write memory
+		.rob_dispatch_en_i,//signal from dispatch to allocate entry in rob
+		.decode2rob_br_mask_i,
+
+		//----------------------------------------------------------------------
+		//Functional Unit Signal Input
+		//----------------------------------------------------------------------
+		.fu2rob_idx_i,//tag sent from functional unit to know which entry's done register needed to be set 
+		.fu2rob_done_signal_i,//done signal from functional unit 
+		.fu2rob_br_taken_i,//branck taken result sent from functional unit
+
+
+		.rob2rs_tail_idx_o,//tail # sent to rs to record which entry the instruction is 
+		.rob2fl_tag_o,//tag from ROB to freelist for returning the old tag to freelist 
+		.rob2arch_map_tag_o,//tag from ROB to Arch map
+		.rob2arch_map_logic_dest_o,//logic dest from ROB to Arch map
+		.rob_stall_dp_o,//signal show if the ROB is full
+		.rob_head_retire_rdy_o,//the head of ROb is ready to retire
+
+		//----------------------------------------------------------------------
+		//Early Recovery Signal Ouput
+		//----------------------------------------------------------------------
+		.br_recovery_rdy_o,//ready to start early branch recovery
+		.rob2fl_recover_head_o,
+		.br_recovery_mask_o,
+		.br_state_o//branch state signal to branch mask
+	);	
 
 	//===============================================================
 	// map table and free list instantiation
