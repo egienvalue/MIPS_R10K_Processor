877093455cba8fc60a1743a5478fa2db1744a2fa
Jun Luo <juluo@umich.edu>
2017-11-20 22:01:13 -0500

    tested perceptron & critical path 4.2

--- verilog/perceptron/train.v
+++ verilog/perceptron/train.v
@@ -4,44 +4,39 @@ module train (
 		input									br_outcome_i,
 		input	[63:0]							if_br_PC_i,
 		input	[63:0]							fu_br_PC_i,
-		input									y_out_i,
+		input									y_vld_i,						
+		input	signed	[`WEIGHT_W-1:0]					y_out_i,
 		input									training_en_i,
-		input									BHR_i,
-		input	[`BHR_W:0][`WEIGHT_W-1:0]		selected_weight_i,
+		input	[`BHR_W-1:0]					BHR_i,
+		input	signed	[`WEIGHT_W-1:0]			sel_weight_i[`BHR_W:0],
 			
-		output	[`BHR_W:0][`WEIGHT_W-1:0]		new_weight_o,
-		output	[`PT_IDX_W-1:0]					tr2pt_wr_idx_o,
-		output									tr2pt_wr_en_o			
+		output	logic	signed	[`WEIGHT_W-1:0]	new_weight_o[`BHR_W:0],
+		output	logic	[`PT_IDX_W-1:0]					tr2pt_wr_idx_o,
+		output	logic							tr2pt_wr_en_o			
 	
 	);
 
-	logic	signed	[`PT_W-1:0][`WEIGHT_W-1:0]	y_out;
-	logic	signed	[`WEIGHT_W-1:0]				y_out_nxt;
-
+	logic	signed	[`WEIGHT_W-1:0]	y_out[`PT_W-1:0];
+	logic	signed	[`WEIGHT_W-1:0]	y_out_nxt;
+	logic	[`BHR_W:0]	x;	
 	assign tr2pt_wr_en_o	= training_en_i;
-	assign y_out_nxt	= y_out_i;
+	assign y_out_nxt		= (y_vld_i) ? y_out_i : y_out[if_br_PC_i[`PT_IDX_W-1:0]];
 	assign tr2pt_wr_idx_o	= fu_br_PC_i[`PT_W-1:0];
-
+	assign	x	= {BHR_i,1'b1};
 	always_comb begin
-		if (y_out[fu_br_PC_i[`PT_IDX_W-1:0]]<`THRESHOLD) begin
-			for (int i;i<`BHR_W;i++) begin
-				if(br_outcome_i==BHR_i[i])
-					new_weight_o[i] = selected_weight[i]+1;
-			else
-					new_weight_o[i] = selected_weight[i]-1;
-			end
-		end	else begin
-			new_weight_o	= selected_weight_i;
+		for (int i=0;i<=`BHR_W;i++) begin
+			new_weight_o[i] = (~training_en_i) ? 0 :
+							  (br_outcome_i==x[i])? (sel_weight_i[i]+1) : (sel_weight_i[i]-1);
 		end
 	end
-	
+	// synopsys sync_set_reset "rst"
 	always_ff @(posedge clk) begin
-		if(rst)
-			y_out	<= `SD 0;
+		if(rst) begin
+			for (int i=0;i<`PT_W;i++) begin
+				y_out[i]	<= `SD 0;
+			end
+		end
 		else 
 			y_out[if_br_PC_i[`PT_IDX_W-1:0]]	<= `SD y_out_nxt;
 	end
-
-
-
 endmodule
